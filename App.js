4df2884111dc949d0b74e9f1cb6bc4a49f5c76c3
AgustinMassieri <agmassieri00@gmail.com>
2022-10-31 23:59:13 -0300

    MD5 & SHA256 implementation

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { sha256 } from 'js-sha256';
+import md5 from 'md5';
+
 
 export default function App() {
+
+  const [input, setInput] = useState('');
+  const [inputConvertedSHA256, setInputConvertedSHA256] = useState('');
+  const [inputConvertedMD5, setInputConvertedMD5] = useState('');
+
+
+  const onCovert = () => {
+    const aux = sha256(input);
+    setInputConvertedSHA256(aux);
+    const aux2 = md5(input);
+    setInputConvertedMD5(aux2);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontWeight: 'bold', marginBottom: 10}}>Generate SHA256 Encoded Hash</Text>
+      <Text style={{marginBottom: 10}}>Please insert any value to convert in SHA256</Text>
+      <TextInput style={{borderBottomColor: 'black', borderBottomWidth: 2, marginBottom: 10}} placeholder='Insert value:' value={input} onChangeText={ (value) => setInput(value) }/>
+      <TouchableOpacity onPress={onCovert}>
+        <Text style={{borderColor: 'black', borderWidth: 2, margin: 5, borderRadius: 2, padding: 4}}>Convert to SHA256</Text>
+      </TouchableOpacity>
+      <Text style={{color: 'red', marginBottom: 5}}>SHA256: {inputConvertedSHA256}</Text>
+      <Text style={{color: 'red', marginBottom: 5}}>MD5: {inputConvertedMD5}</Text>
+
+    </SafeAreaView>
   );
 }
 
